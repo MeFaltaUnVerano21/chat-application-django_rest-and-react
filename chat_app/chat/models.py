1596345ec267dbb37f3efce78189f3194ebd1faf
MeFaltaUnTelediario <7711840-coderty@users.noreply.gitlab.com>
2021-05-06 17:52:51 +0200

    Django_Rest install

--- chat_app/chat/models.py
+++ chat_app/chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.TextField(default="", max_length=20, null="")
+    email = models.CharField(default="", max_length=20, unique=True, null="")
+    url = models.CharField(default="", max_length=200, unique=False, null=False)
--- chat_app/chat/views.py
+++ chat_app/chat/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import generics, status
+from .serializers import UserSerializer
+from .models import User
 
 # Create your views here.
+class UserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
